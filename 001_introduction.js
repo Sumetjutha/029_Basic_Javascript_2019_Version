0dfa35215f03152227fe547fcbba195fb4d308ad
Sumet Jutajan <sumet.jutha@gmail.com>
2023-01-28 01:50:07 +0700

    add files

--- 001_introduction.js
+++ 001_introduction.js
@@ -1,4 +1,5 @@
 console.log("Hello");
 console.log("Fuck you oh yeah!!!");
 
-// comment this is not show
+// comment this is not show-
+console.log("Lemon Honey Soda");
---
+++ 002_variables.js
@@ -0,0 +1,6 @@
+// a variables is just something that lets us store a value
+let myfavoritenumber = 8;
+console.log(myfavoritenumber);
+
+myfavoritenumber = 22;
+console.log(myfavoritenumber);
