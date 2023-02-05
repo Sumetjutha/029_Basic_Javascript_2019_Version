8587f257786a7a19fc6a468642ae0daa90a54d72
Sumet Jutajan <sumet.jutha@gmail.com>
2023-02-05 22:27:22 +0700

    Create 003_declare_and_assignment.js

---
+++ 003_declare_and_assignment.js
@@ -0,0 +1,7 @@
+// a variable can be declare by using the keyword "let"
+let somefunnything;
+console.log(somefunnything);
+somefunnything = -54;
+console.log(somefunnything);
+somefunnything = -23;
+console.log(somefunnything);
